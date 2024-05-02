cb76ef5b1c452612f56ac31616d7ee9d4e90bf51
Raj Meena <heyrajmeena@gmail.com>
2024-05-02 21:58:03 +0530

    Project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,20 @@
+export const signup= async(req, res)=>{
+    res.json({
+        data: "You hit the signup point.",
+    });
+}
+
+
+export const login= async(req, res)=>{
+    res.json({
+        data: "You hit the login point.",
+    });
+}
+
+
+export const logout= async(req, res)=>{
+    res.json({
+        data: "You hit the logout point.",
+    });
+}
+
---
+++ backend/db/connectMongoDB.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const connectMongoDB=async()=>{
+    try {
+        const connect=await mongoose.connect(process.env.MONGO_URI)
+        console.log(`MongoDB Connected: ${connect.connection.host}`)
+    } catch (error) {
+        console.log(`Error while connecting MongoDB: ${error.message}`);
+        process.exit(1);
+    }
+}
+export default connectMongoDB;
