4a558eb7c84361ee873443b78b9b52c239edcee0
Raj Meena <heyrajmeena@gmail.com>
2024-08-28 14:48:19 +0530

    3 - Payload limit updated in server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ cloudinary.config({
 });
 const app=express();
 const PORT=process.env.PORT
-app.use(express.json());
+app.use(express.json({limit:"500kb"}));
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
