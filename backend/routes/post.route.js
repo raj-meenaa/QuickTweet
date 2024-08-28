c8670455f65bbe22be351f780c09403d75e25d22
Raj Meena <heyrajmeena@gmail.com>
2024-08-28 15:14:44 +0530

    3 - like unlike post method added

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,19 +1,25 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { commentOnPost, createPost, deletePost, getAllPost, getFollowingPost, getLikedPosts, getUserPosts, likeUnlikePost } from "../controllers/post.controller.js";
+import {
+	commentOnPost,
+	createPost,
+	deletePost,
+	getAllPosts,
+	getFollowingPosts,
+	getLikedPosts,
+	getUserPosts,
+	likeUnlikePost,
+} from "../controllers/post.controller.js";
 
+const router = express.Router();
 
-const router=express.Router();
-
-
-router.get("/allpost", protectRoute, getAllPost);
-router.get("/following", protectRoute, getFollowingPost);
+router.get("/all", protectRoute, getAllPosts);
+router.get("/following", protectRoute, getFollowingPosts);
 router.get("/likes/:id", protectRoute, getLikedPosts);
 router.get("/user/:username", protectRoute, getUserPosts);
-router.post("/create", protectRoute, createPost)
-router.post("/like/:id", protectRoute, likeUnlikePost)
-router.post("/comment/:id", protectRoute, commentOnPost)
-router.delete("/:id", protectRoute, deletePost)
+router.post("/create", protectRoute, createPost);
+router.post("/like/:id", protectRoute, likeUnlikePost);
+router.post("/comment/:id", protectRoute, commentOnPost);
+router.delete("/:id", protectRoute, deletePost);
 
-
-export default router
+export default router;
