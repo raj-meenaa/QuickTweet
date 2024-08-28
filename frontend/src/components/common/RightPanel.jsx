3c15abff29fdb2d061ce9e036499a01c9ddb780a
Raj Meena <heyrajmeena@gmail.com>
2024-08-28 15:03:39 +0530

    3 - useFollow hook added

--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 
-// import useFollow from "../../hooks/useFollow";
+import useFollow from "../../hooks/useFollow";
 
 import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
 import LoadingSpinner from "./LoadingSpinner";
@@ -23,7 +23,7 @@ const RightPanel = () => {
 		},
 	});
 
-	// const { follow, isPending } = useFollow();
+	const { follow, isPending } = useFollow();
 
 	if (suggestedUsers?.length === 0) return <div className='md:w-64 w-0'></div>;
 
